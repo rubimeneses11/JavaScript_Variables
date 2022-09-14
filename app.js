0da4dbef789e3788eea5738e8ec5ce8c395ea9ef
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-14 14:39:51 -0500

    done with exercise

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Rubi";
+let lastName = "Meneses";
+var age = 25;
 // Exercise 2
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${language} was first released in ${createdYear}.`)
+
+console.log(`${title} by ${author} is ${pageCount} pages long.`)
